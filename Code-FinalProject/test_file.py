1b0b40affca5ecf82180ef7c392e36bd355a474d
liuyarong <yliu255@stevens.edu>
2019-04-26 13:13:57 -0400

    Test code

--- Code-FinalProject/test_file.py
+++ Code-FinalProject/test_file.py
@@ -1,3 +1,9 @@
+# Author: Yarong Liu
+# Function: Test if the main program run successfully（Because pytest cannot run input() function,
+# I just creat a new python file First_two.py and the only difference between two files is that
+# all the input information in First_one.py has been changed to immutable information in First_two.py)
+
+
 import pytest
 from First_two import Auto_order
 
